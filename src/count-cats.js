5ba3084a91365737846e0c72f3bd553fb7fc577a
Raman Karseka <ier@live.ru>
2021-03-26 01:25:20 +0300

    feat: task Count cats!

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+    return arr.reduce((previousValue, item) => previousValue + item.reduce((pv, i) => (i === '^^')? pv + 1:pv, 0), 0)
 };
