f96b68d4ffa05d56a215f7262b353ceace9629e3
Raman Karseka <ier@live.ru>
2021-03-26 02:08:36 +0300

    feat: task What season?

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(arr) {
-    return arr.reduce((previousValue, item) => previousValue + item.reduce((pv, i) => (i === '^^')? pv + 1:pv, 0), 0)
+    return arr.reduce((previousValue, item) => previousValue + item.reduce((pv, i) => (i === '^^') ? pv + 1 : pv, 0), 0)
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(inc) {
+    if (inc === null)
+        throw new CustomError('THROWN');
+    if (isNaN(Date.parse(inc)))
+        throw new CustomError('THROWN');
+    const springDate = new Date(inc);
+    const arr = [
+        'winter',
+        'winter',
+        'spring',
+        'spring',
+        'spring',
+        'summer',
+        'summer',
+        'summer',
+        'fall',
+        'fall',
+        'fall',
+        'winter',
+    ];
+    //
+    return arr[springDate.getMonth()];
 };
