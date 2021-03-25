95f982b05ca22cfa26a41b67c959d82b295644bb
Raman Karseka <ier@live.ru>
2021-03-26 01:04:37 +0300

    feat: task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(arr) {
+if ( !Array.isArray(arr))
+	return false;
+
+  return arr.map(i=>(typeof i==="string")?i.trim().substr(0,1).toUpperCase():'').sort().join('');
+
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
