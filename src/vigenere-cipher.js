0d0051a8c6d41d54f1e6e916204d61610598de98
Raman Karseka <ier@live.ru>
2021-03-30 00:39:25 +0300

    feat: task Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,17 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-    encrypt = (str = false, key = false) => {
-        if (!str || !key)
-            throw new Error();
-        return str;
-    }
-    decrypt = (str = false, key = false) => {
-        if (!str || !key)
-            throw new Error();
-        return str;
-        // remove line with error and write your code here
-    }
+  constructor(prop = true) {
+    this.direction = prop;
+  }
+  encrypt(str = false, key = false) {
+    if (!str || !key) throw new Error();
+    key = key
+      .toUpperCase()
+      .repeat(Math.ceil(str.length / key.length))
+      .slice(0, str.length);
+    str = str
+      .toUpperCase()
+      .split("")
+      .reduce(
+        (pv, item) => {
+          let itemCode = item.charCodeAt();
+          if (itemCode > 64 && itemCode < 91) {
+            let newItemCode = itemCode - 65 + key[pv[1]].charCodeAt();
+            return [
+              [
+                ...pv[0],
+                String.fromCharCode(
+                  newItemCode > 90 ? newItemCode - 26 : newItemCode
+                ),
+              ],
+              pv[1] + 1,
+            ];
+          } else return [[...pv[0], item], pv[1]];
+        },
+        [[], 0]
+      )[0];
+    if (this.direction) return str.join("");
+    else return str.reverse().join("");
+  }
+  decrypt(str = false, key = false) {
+    if (!str || !key) throw new Error();
+    key = key
+      .toUpperCase()
+      .repeat(Math.ceil(str.length / key.length))
+      .slice(0, str.length);
+    str = str.split("").reduce(
+      (pv, item) => {
+        let itemCode = item.charCodeAt();
+        if (itemCode > 64 && itemCode < 91) {
+          let newItemCode = itemCode + 65 - key[pv[1]].charCodeAt();
+          return [
+            [
+              ...pv[0],
+              String.fromCharCode(
+                newItemCode < 65 ? newItemCode + 26 : newItemCode
+              ),
+            ],
+            pv[1] + 1,
+          ];
+        } else return [[...pv[0], item], pv[1]];
+      },
+      [[], 0]
+    )[0];
+    if (this.direction) return str.join("");
+    else return str.reverse().join("");
+  }
 }
 
 module.exports = VigenereCipheringMachine;
